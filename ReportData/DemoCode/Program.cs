0777a1e4239f675ac259d74a2c1e2cb389dd25c2
hilliverson <hilliverson@gmail>
2018-07-23 13:59:43 +0800

    新增DemoCode

---
+++ ReportData/DemoCode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetHeight(5);
+            rectangle.SetWidth(3);
+            Console.WriteLine(rectangle.GetWidth()*rectangle.GetHeight());
+
+            Sqaure sqaure = new Sqaure();
+            sqaure.SetHeight(5);
+            sqaure.SetWidth(3);
+            Console.WriteLine(sqaure.GetWidth() * sqaure.GetHeight());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ReportData/DemoCode/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    class Rectangle
+    {
+        private int _Width;
+        private int _Height;
+
+        public virtual void SetWidth(int Value)
+        {
+            this._Width = Value;
+        }
+
+        public virtual void SetHeight(int Value)
+        {
+            this._Height = Value;
+        }
+
+        public int GetWidth()
+        {
+            return _Width;
+        }
+
+        public int GetHeight()
+        {
+            return _Height;
+        }
+
+    }
+
+    class Sqaure : Rectangle
+    {
+        public override void SetHeight(int Value)
+        {
+            base.SetHeight(Value);
+            base.SetWidth(Value);
+        }
+
+        public override void SetWidth(int Value)
+        {
+            base.SetWidth(Value);
+            base.SetHeight(Value);
+        }
+
+    }
+}
